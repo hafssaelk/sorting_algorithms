67939e3893fc86ee89abc9c6bd7d9377eee6c52a
hafssaelk <hafssa.elkoi0@gmail.com>
2023-12-17 04:45:05 -0800

    r sort

---
+++ 105-radix_sort.c
@@ -0,0 +1,87 @@
+#include "sort.h"
+
+/**
+ * get_max - Get the maximum value in an array of integers.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Return: The maximum integer in the array.
+ */
+int get_max(int *array, int size)
+{
+	int max, i;
+
+	for (max = array[0], i = 1; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+
+	return (max);
+}
+
+/**
+ * radix_counting_sort - Perform counting sort on the significant digits of
+ *                       an array of integers in ascending order.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ * @sig: The significant digit to sort on.
+ * @buff: A buffer to store the sorted array.
+ */
+void radix_counting_sort(int *array, size_t size, int sig, int *buff)
+{
+	int count[10] = {0};
+	size_t i;
+
+	/* Count occurrences of each digit at the current significant place */
+	for (i = 0; i < size; i++)
+		count[(array[i] / sig) % 10] += 1;
+
+	/* Modify the count array to store the cumulative count */
+	for (i = 1; i < 10; i++)
+		count[i] += count[i - 1];
+
+	/* Build the output array by placing elements in their correct positions */
+	for (i = size - 1; (int)i >= 0; i--)
+	{
+		buff[count[(array[i] / sig) % 10] - 1] = array[i];
+		count[(array[i] / sig) % 10] -= 1;
+	}
+
+	/* Copy the sorted elements back to the original array */
+	for (i = 0; i < size; i++)
+		array[i] = buff[i];
+}
+
+/**
+ * radix_sort - Sort an array of integers in ascending order using
+ *              the LSD (Least Significant Digit) radix sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Implements the LSD radix sort algorithm. Prints
+ * the array after each increase in significant digits.
+ */
+void radix_sort(int *array, size_t size)
+{
+	int max, sig, *buff;
+
+	if (array == NULL || size < 2)
+		return;
+
+	buff = malloc(sizeof(int) * size);
+	if (buff == NULL)
+		return;
+
+	max = get_max(array, size);
+
+	/* Perform LSD radix sort for each significant digit place */
+	for (sig = 1; max / sig > 0; sig *= 10)
+	{
+		radix_counting_sort(array, size, sig, buff);
+		print_array(array, size); /* Print the array after each pass */
+	}
+
+	free(buff);
+}
+
